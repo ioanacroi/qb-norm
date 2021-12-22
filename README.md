e83c9bc6c5b98bb5408fea0d07f910f92dbd34a1
Ioana Croitoru <ioana.croi@gmail.com>
2021-12-22 14:59:56 +0000

    QB-Norm

---
+++ README.md
@@ -0,0 +1,6 @@
+**QB-Norm Results on MSRVTT Benchmark**
+
+| Model | Split | Task | R@1 | R@5 | R@10 | R@50 | MdR | Geom | Links |
+| ----- | ------| ---- | --- | --- | ---- | ---- | --- | --- | ----- |
+| TT-CE+    | Full  | t2v  | <sub><sup>14.9<sub>(0.1)</sub></sup></sub> | <sub><sup>38.3<sub>(0.1)</sub></sup></sub> | <sub><sup>51.5<sub>(0.1)</sub></sup></sub> | <sub><sup>10.0<sub>(0.0)</sub></sup></sub> | <sub><sup>30.9<sub>(0.1)</sub></sup></sub> | |
+| TT-CE+ (+QB-Norm) | Full | t2v | <sub><sup>17.3<sub>(0.0)</sub></sup></sub> | <sub><sup>42.1<sub>(0.2)</sub></sup></sub> | <sub><sup>54.9<sub>(0.1)</sub></sup></sub> | <sub><sup>8.0<sub>(0.0)</sub></sup></sub> | <sub><sup>34.2<sub>(0.1)</sub></sup></sub> | |
---
+++ dynamic_inverted_softmax.py
@@ -0,0 +1,65 @@
+import argparse
+import pickle as pkl
+import numpy as np
+import metric
+
+# Returns list of retrieved top k videos based on the sims matrix
+def get_retrieved_videos(sims, k):
+    argm = np.argsort(-sims, axis=1)
+    topk = argm[:,:k].reshape(-1)
+    retrieved_videos = np.unique(topk)
+    return retrieved_videos
+
+# Returns list of indices to normalize from sims based on videos
+def get_index_to_normalize(sims, videos):
+    argm = np.argsort(-sims, axis=1)[:,0]
+    result = np.array(list(map(lambda x: x in videos, argm)))
+    result = np.nonzero(result)
+    return result
+
+
+def qb_norm(train_test, test_test, args):
+    k = args.get("k", 1)
+    beta = args.get("beta", 20)
+    retrieved_videos = get_retrieved_videos(train_test, k)
+    train_test = np.exp(train_test*beta)
+    test_test = np.exp(test_test*beta)
+
+    normalizing_sum = np.sum(train_test, axis=0)
+    index_for_normalizing = get_index_to_normalize(test_test, retrieved_videos)
+    test_test_normalized = test_test
+    test_test_normalized[index_for_normalizing, :] = np.divide(test_test[index_for_normalizing, :], normalizing_sum)
+    return test_test_normalized
+
+def main():
+    args = argparse.ArgumentParser(description='QB-Norm and Dynamic Inverted Softmax')
+    args.add_argument("--train_captions_test_videos_path", 
+            type=str, required=True,
+            help="path to the similarity matrix between the captions from training and the videos from testing")
+    args.add_argument("--test_captions_test_videos_path", 
+            type=str, required=True,
+            help="path to the similarity matrix between the captions from testing and the videos from testing, this is the original matrix used for computing the metrics")
+    args.add_argument("--test_query_masks_path",
+            type=str, default=None,
+            help="path to the query masks")
+    args.add_argument("--beta", default=20.0, type=float)
+    args.add_argument("--k", default=1, type=float)
+    args = vars(args.parse_args())
+
+    train_test = pkl.load(open(args["train_captions_test_videos_path"], 'rb'))
+    test_test = pkl.load(open(args["test_captions_test_videos_path"], 'rb'))
+    test_query_masks_path = args.get("test_query_masks_path", None)
+    if test_query_masks_path:
+        test_query_masks = pkl.load(open(test_query_masks_path, 'rb'))
+    else:
+        test_query_masks = None
+
+    print("Metrics before applying QB-Norm")
+    print(metric.t2v_metrics(test_test, test_query_masks))
+
+    test_test_normalized = qb_norm(train_test, test_test, args)
+    print("Metrics after QB-Norm")
+    print(metric.t2v_metrics(test_test_normalized, test_query_masks))
+
+if __name__ == '__main__':
+    main()
